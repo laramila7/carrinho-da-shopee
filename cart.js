fed25470fed9eb8f7fcfe3ed2d90d0c7c73fb45a
laramila7 <laramilarindo6@gmail.com>
2026-04-28 10:26:52 -0300

    Update cart.js

--- cart.js
+++ cart.js
@@ -4,7 +4,7 @@
 //dicionar item no carrinnho
 
 async function addItem(userCart) {
-
+  userCart.push(item);
 }
 
 //deletar item do carrinho
@@ -19,5 +19,7 @@ async function removerItem(userCart, index){
 
 //calcular o total do carrinho
 async function calculateTotal(userCart){
-
+return userCart.reduce(total, item) => total + item.subtotal(), 0);
 }
+
+
