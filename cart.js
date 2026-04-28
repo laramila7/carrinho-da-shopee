0e2fdc9ce348b561adbb532ff8ca9c43d9d31712
laramila7 <laramilarindo6@gmail.com>
2026-04-28 10:08:47 -0300

    Update cart.js

---
+++ cart.js
@@ -0,0 +1,23 @@
+//quais ações meu carrinho pode fazer
+
+//cosos de uso
+//dicionar item no carrinnho
+
+async function addItem(userCart) {
+
+}
+
+//deletar item do carrinho
+async function deleteItem(userCart, name){
+
+}
+
+//remover um item do carrinho
+async function removerItem(userCart, index){
+
+}
+
+//calcular o total do carrinho
+async function calculateTotal(userCart){
+
+}
