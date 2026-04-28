a6987d7b8fb8bc7e3db5fd883d2385539a3f01a5
laramila7 <laramilarindo6@gmail.com>
2026-04-28 11:04:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ console.log("welcome to the your Shopee Cart!");
 const item1 = await createItem("hotwheels ferrari", 20.99, 1);
 const item2 = await createItem(hotwheels lamborghini", 39.99, 3);
 
+await cartService.addItem(myCart, item1);
+await cartService.addItem(myCart, item2);
+
+await 
 console.log(item2.subtotal());
