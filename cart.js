14047b4f21b555136b9a65052eebf97ae1ae95f8
laramila7 <laramilarindo6@gmail.com>
2026-05-04 11:46:15 -0300

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,35 +1,64 @@
-//quais ações meu carrinho pode fazer
+//quais açoes meu carrinho pode fazer
 
-//cosos de uso
-//dicionar item no carrinnho
-
-async function addItem(userCart) {
+//CASOS DE USO
+// ✅ -> adicionar item no carrinho
+async function addItem(userCart, item) {
   userCart.push(item);
 }
 
-//deletar item do carrinho
-async function deleteItem(userCart, name){
-  const index = userCart.findIndex((item) =>) item.name === name);
+// ✅ -> calcular o total do carrinho
+async function calculateTotal(userCart) {
+  console.log("\nShopee Cart TOTAL IS:");
 
-  if(index
-  
+  const result = userCart.reduce((total, item) => total + item.subtotal(), 0);
+  console.log(`🎁Total: ${result}`);
 }
 
-//remover um item do carrinho
-async function removerItem(userCart, index){
+// -> deletar item do carrinho
+async function deleteItem(userCart, name) {
+  const index = userCart.findIndex((item) => item.name === name);
 
+  if (index !== -1) {
+    userCart.splice(index, 1);
+  }
 }
 
-//calcular o total do carrinho
-async function calculateTotal(userCart){
-return userCart.reduce(total, item) => total + item.subtotal(), 0);
+// -> ✅ remover um item - diminui um item
+async function removeItem(userCart, item) {
+  //1. encontrar o indice do item
+  const indexFound = userCart.findIndex((p) => p.name === item.name);
+
+  //2. Caso não encontre o item
+  if (indexFound == -1) {
+    console.log("item não encontrado");
+    return;
+  }
+
+  //3. item > 1 subtrair um item
+  if (userCart[indexFound].quantity > 1) {
+    userCart[indexFound].quantity -= 1;
+    return;
+  }
+
+  //4. caso item = 1 deletar o item
+  if (userCart[indexFound].quantity == 1) {
+    userCart.splice(indexFound, 1);
+    return;
+  }
 }
 
-export {
-    addItem,
-    calculateTotal,
-    deleItem,
+// ✅ mostra todos os items do carrinho
+async function displaycart(userCart) {
+  console.log("\nShopee cart list:");
+  userCart.forEach((item, index) => {
+    console.log(
+      `${index + 1}. ${item.name} - R$ ${item.price} | ${
+        item.quantity
+      }x | Subtotal = ${item.subtotal()}`
+    );
+  });
 }
-  
+
+export { addItem, calculateTotal, deleteItem, removeItem, displaycart };
 
 
